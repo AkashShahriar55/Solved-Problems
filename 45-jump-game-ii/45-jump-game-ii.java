27b09070b244d93e6df0a108f432772ea833ae31
Akash Shahriar <49646605+AkashShahriar55@users.noreply.github.com>
2022-07-04 21:17:07 +0600

    Time: 32 ms (41.86%), Space: 50.9 MB (5.89%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        int len = nums.length;
+        if(len <= 1)
+            return 0;
+        
+        int step = 0;
+        int ptr = 0;
+        while(true){
+            System.out.println(ptr + " " + step);
+            int jumpSize = nums[ptr];
+            
+            int maxPosition = -1;
+            int maxNumber = Integer.MIN_VALUE;
+            
+            if(ptr+jumpSize >= len-1)
+                return ++step;
+            
+            
+            for(int j = 0; j < jumpSize ; j++ ){
+                ptr++;
+                
+                if(nums[ptr]-jumpSize+j+1 >= maxNumber){
+                    maxNumber = nums[ptr]-jumpSize+j+1;
+                    maxPosition = ptr;
+                }
+                
+            }
+            
+            ptr = maxPosition;
+            step++;
+        }
+        
+        
+    }
+}
